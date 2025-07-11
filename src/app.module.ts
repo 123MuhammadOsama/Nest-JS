e2f9e3cd16daeff65d54d2e0a127fe587f9346df
Arham Baig <143251008+ScriptersLtd@users.noreply.github.com>
2025-07-11 13:37:59 +0500

    Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
+import { EmpoloyeeModule } from './empoloyee/empoloyee.module';
+import { EmployeeService } from './employee/employee.service';
+import { EmployeeController } from './employee/employee.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController, ProductController],
-  providers: [AppService, ProductService],
+  imports: [EmpoloyeeModule],
+  controllers: [AppController, UserController, ProductController, EmployeeController],
+  providers: [AppService, ProductService, EmployeeService],
 })
 export class AppModule {}
